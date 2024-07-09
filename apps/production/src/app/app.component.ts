9a2adec446a45ecb17afd6a85576d229878bb844
aosenit <creativitytraz22@gmail.com>
2024-07-09 02:59:45 +0100

    first push

--- apps/production/src/app/app.component.ts
+++ apps/production/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { SharedComponentsComponent } from '@my-monorepo/shared-components';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, SharedComponentsComponent],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'auth';
+  title = 'production';
 }
